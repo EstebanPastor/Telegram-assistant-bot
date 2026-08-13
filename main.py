dee3ce90458e0c8208e84eb9512efde8fb7def2e
esteban.pastor <estebanandrespastor@gmail.com>
2026-08-13 11:35:18 -0300

    add fix with ports

--- main.py
+++ main.py
@@ -1,15 +1,34 @@
 import os
+import threading
+from http.server import HTTPServer, BaseHTTPRequestHandler
 from dotenv import load_dotenv
 from google import genai
 from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, MessageHandler, filters
 
+class SimpleHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"Bot OK")
 
+    def log_message(self, format, *args):
+        
+        return
+
+def run_dummy_server():
+    port = int(os.environ.get("PORT", 10000))
+    server = HTTPServer(("0.0.0.0", port), SimpleHandler)
+    server.serve_forever()
+
+# Levantar servidor en segundo plano
+threading.Thread(target=run_dummy_server, daemon=True).start()
+
+# --- CÓDIGO DE TU BOT ---
 load_dotenv()
 
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
-
 
 client = genai.Client(api_key=GEMINI_API_KEY)
 
@@ -21,7 +40,6 @@ async def responder(update: Update, context: ContextTypes.DEFAULT_TYPE):
         return
 
     try:
-      
         response = client.models.generate_content(
             model='gemini-2.5-flash',
             contents=user_text,
