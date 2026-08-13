1ea97f77212ff95e504dc94a3ef7fe2faefd2c20
esteban.pastor <estebanandrespastor@gmail.com>
2026-08-13 12:16:12 -0300

    add auto-clean for session cache duplicated

--- main.py
+++ main.py
@@ -33,8 +33,8 @@ GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
 client = genai.Client(api_key=GEMINI_API_KEY)
 
-
 user_sessions = {}
+
 
 
 async def responder(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -46,16 +46,13 @@ async def responder(update: Update, context: ContextTypes.DEFAULT_TYPE):
         return
 
     try:
-        
         if user_id not in user_sessions:
             user_sessions[user_id] = client.chats.create(model='gemini-2.5-flash')
 
         chat = user_sessions[user_id]
 
-       
         await context.bot.send_chat_action(chat_id=update.effective_chat.id, action="typing")
 
-       
         response = await asyncio.to_thread(chat.send_message, user_text)
         bot_reply = response.text
 
@@ -63,7 +60,6 @@ async def responder(update: Update, context: ContextTypes.DEFAULT_TYPE):
         print(f"Error de Python/Gemini: {e}")
         bot_reply = f"Error al procesar la respuesta: {e}"
 
-    
     max_length = 4000
     if len(bot_reply) > max_length:
         for i in range(0, len(bot_reply), max_length):
@@ -77,4 +73,6 @@ if __name__ == '__main__':
     print("Iniciando bot con memoria...")
     app = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
     app.add_handler(MessageHandler(filters.TEXT & (~filters.COMMAND), responder))
-    app.run_polling()
+    
+
+    app.run_polling(drop_pending_updates=True)
